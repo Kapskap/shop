a4e85f7423ff4f1bc06ecdf6e30f8501251304eb
Krzysztof Sulej <kaps@o2.pl>
2025-02-04 11:55:13 +0100

    Dodano wyświetlanie klienta

---
+++ src/Controller/CustomerController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\CustomerRepository;
+use App\Entity\Customer;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation;
+
+class CustomerController extends AbstractController
+{
+    #[Route('/customer/{id}', name: 'show_customer')]
+    public function getCustomer($id, EntityManagerInterface $entityManager): Response
+    {
+        $customer = $entityManager->getRepository(Customer::class)->find($id);
+        if (!$customer) {
+            throw $this->createNotFoundException('Nie znaleziono id '.$id);
+        }
+
+        return $this->render('customer/show.html.twig', [
+            'customer'=>$customer,
+            ]);
+    }
+
+}
